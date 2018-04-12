f805232d62562cbe200ba70a840ddc94b9d3e9c6
Herve ROLLAND <h.rolland@lefebvre-sarrut.eu>
2018-04-12 07:43:11 +0200

    Second version with some comment

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Monte Carlo Tree Search
 
-Premier essai.
+Propose une mise en oeuvre en Scala, inspiré de <http://www.baeldung.com/java-monte-carlo-tree-search>,
+pour le jeu [Ultimate TicTacToe de Codingame](https://www.codingame.com/multiplayer/bot-programming/tic-tac-toe).
 
+La première version avait le même niveau que ma mise en oeuvre utilisant un MiniMax/AlphaBeta.
 
+La deuxième version avec un réglage du score pour prendre en compte les parties perdues et les parties nulles 
+m'a permi de gagner 15 places.
--- src/main/scala/mcts/MonteCarloSearchTree.scala
+++ src/main/scala/mcts/MonteCarloSearchTree.scala
@@ -2,17 +2,53 @@ package mcts
 
 import scala.util.Random
 
+/**
+	* Represents a state of the game
+	*/
 trait Board {
+	/** Indicate who play next, true if me, false if opponent. */
 	val meOrOpponent: Boolean
+	/**
+		* Returns some score if final, none if not final.<br/>
+		* The score must be positive if player who play win, 0 if draw, negative if player who play loose.<br/>
+		* The player who play is inverse than meOrOpponent.
+		*/
 	val optScoreIfFinal: Option[Int]
-	def randomNext(random: Random): Board
+	/**
+		* Do a random play with the specified random.
+		* @param random the specified random
+		* @return the final score
+		*/
+	def randomPlay(random: Random): Int
+	/**
+		* @return all possible game state when [[Board.meOrOpponent]] play
+		*/
 	def getAllPossibleNextBoard: Seq[Board]
 }
 
+/**
+	* Represents a studied node.
+	* @param board the [[Board]]
+	* @param parent the parent node
+	* @param subNodes the sub nodes
+	* @param nbVisit the number of visit
+	* @param score the score
+	*/
 class Node(val board: Board, val parent: Node = null, var subNodes: Seq[Node] = Seq.empty, var nbVisit: Int = 0, var score: Int = 0)
 
+/**
+	* The Monte Carlo tree search implementation.
+	*/
 object MonteCarloTreeSearch {
+	/** The random instance. */
 	val random: Random = new Random(System.currentTimeMillis())
+	/**
+		* Find best next move from the specified board and the max duration.<br/>
+		* Implements the Monte Carlo Tree Search
+		* @param board the specified board
+		* @param uctsTime the max duration
+		* @return the best board
+		*/
 	def findNextMove(board: Board, uctsTime: Int): Board = {
 		val end = System.currentTimeMillis() + uctsTime
 		val rootNode = new Node(board)
@@ -36,6 +72,11 @@ object MonteCarloTreeSearch {
 		Console.err.println("found " + bestNode.score + " / " + bestNode.nbVisit + " in " + nbTurn)
 		bestNode.board
 	}
+	/**
+		* Select the promise node = the node to study
+		* @param rootNode the root node
+		* @return the promise node
+		*/
 	def selectPromiseNode(rootNode: Node): Node = {
 		var node = rootNode
 		while (node.subNodes.nonEmpty) {
@@ -50,36 +91,65 @@ object MonteCarloTreeSearch {
 		}
 		node
 	}
+	/**
+		* Expand the specified node.
+		* @param node the specified node
+		*/
 	def expandNode(node: Node): Unit = {
 		node.subNodes = node.board.getAllPossibleNextBoard
 			.map{ board =>
 				new Node(board, node)
 			}
 	}
+	/**
+		* Simulate a random game from the specified node.
+		* @param node the specified node
+		* @return the score of the random game
+		*/
 	def simulateRandomPlayout(node: Node): Int = {
 		if (node.board.optScoreIfFinal.exists(s => s < 0)) {
 			node.parent.score = Int.MinValue
 			-1
 		} else {
-			var currentBoard = node.board
-			while (currentBoard.optScoreIfFinal.isEmpty) {
-				currentBoard = currentBoard.randomNext(random)
-			}
-			currentBoard.optScoreIfFinal.get
+			node.board.randomPlay(random)
 		}
 	}
+	/**
+		* Update tree node (nbVisit and score) from the specified node with the specified game result
+		* @param node the specified node
+		* @param playout the specified game result
+		*/
 	def backPropogation(node: Node, playout: Int): Unit = {
 		var currentNode = node
 		while (currentNode != null) {
 			currentNode.nbVisit += 1
-			if ((playout > 0 && !currentNode.board.meOrOpponent) || (playout < 0 && currentNode.board.meOrOpponent)) {
-				if (currentNode.score != Int.MinValue) {
-					currentNode.score += 1
+			if (currentNode.score != Int.MinValue) {
+				if (currentNode.board.meOrOpponent) {
+					if (playout < 0) {
+						currentNode.score += 3
+					} else if (playout > 0) {
+						currentNode.score -= 1
+					} else {
+						currentNode.score += 1
+					}
+				} else {
+					if (playout > 0) {
+						currentNode.score += 3
+					} else if (playout < 0) {
+						currentNode.score -= 1
+					} else {
+						currentNode.score += 1
+					}
 				}
 			}
 			currentNode = currentNode.parent
 		}
 	}
+	/**
+		* Display in console.err the specified node with the specified prefix.
+		* @param node the specified node
+		* @param prefix the specified prefix
+		*/
 	private def logTree(node: Node, prefix: String = ""): Unit = {
 		Console.err.println(prefix + "(" + node.board.toString + ") " + node.score + "/" + node.nbVisit)
 		val newPrefix = prefix + " "
